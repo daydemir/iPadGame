0d868c9eea644ccc94fd2f4ce2a8b3f06008b085
wang_t <wang_t@macpro-stotts.cs.unc.edu>
2013-04-17 22:29:15 -0400

    a few test methods added

--- iPadGTests/iPadGTests.m
+++ iPadGTests/iPadGTests.m
@@ -12,17 +12,25 @@
 #import "Content.h"
 #import "Grid.h"
 #import "Specs.h"
+#import "MathGame.h"
 
 @implementation iPadGTests
+iPadGameViewController *ipadgamevc;
+GameplayViewController *gameplayvc;
+Content *cont;
+Grid *gr;
+Specs *sp;
+MathGame *ma;
 
 - (void)setUp
 {
     [super setUp];
-    iPadGameViewController *ipadgamevc = [[iPadGameViewController alloc] init];
-    GameplayViewController *gameplayvc = [[GameplayViewController alloc] init];
-    Content *cont = [[Content alloc] initWithWord:@"TEST WORD"];
-    Grid *gr = [[Grid alloc] init];
-    Specs *sp = [[Specs alloc] init];
+    ipadgamevc = [[iPadGameViewController alloc] init];
+    gameplayvc = [[GameplayViewController alloc] init];
+    cont = [[Content alloc] initWithWord:@"TEST WORD"];
+    gr = [[Grid alloc] init];
+    sp = [[Specs alloc] init];
+    ma = [[MathGame alloc] init];
     
 }
 
@@ -33,11 +41,36 @@
     [super tearDown];
 }
 
-- (void)testExample
+- (void)testContent
 {
     [self setUp];
-    STFail(@"Unit tests are not implemented yet in iPadGTests");
+    STAssertNotNil(cont, @"Content object hasn't been created");
+    int num = 90;
+    [cont setMatchID:&num];
+    STAssertEquals(num, (int)*[cont matchID], @"ids are not equal");
+
+    //STFail(@"Unit tests are not implemented yet in iPadGTests");
     //NSLog(@"%@",[cont word]);
 }
+
+- (void)testMath
+{
+    STAssertNotNil(ma, @"MathGame object hasn't been created");
+    NSMutableArray *testArray = [[NSMutableArray alloc] initWithArray:[MathGame generateGrid:8 withDifficulty:1000 withOp:'b']];
+    for (int i =0; i<8;i++){
+        for(int j =0; j<8;j++){
+            if ([[testArray objectAtIndex:i] isEqualToString:[testArray objectAtIndex:j]] && i!=j){
+                STFail(@"there exists a pair of same number");
+            }
+        }
+    }
+                                
+
+    
+}
+
+
+
+
 
 @end
