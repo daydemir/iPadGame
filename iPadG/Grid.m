7cb2727c525500a4a00557ca14b0beb91d67edd7
wang_t <wang_t@macpro-stotts.cs.unc.edu>
2013-04-19 19:04:52 -0400

    all game type added. sounds in a folder

--- iPadG/Grid.m
+++ iPadG/Grid.m
@@ -81,8 +81,10 @@
             //////////////////////////////////loading words from list/////////////////////////////
             NSMutableArray *words;
             NSMutableArray *words_chosen;
+            NSMutableArray *selectedSet;
             words = [NSMutableArray array];
             words_chosen = [NSMutableArray array];
+            selectedSet = [NSMutableArray array];
             //read file
             NSString* path = [[NSBundle mainBundle] pathForResource:@"words"
                                                              ofType:@"lst"];
@@ -97,6 +99,15 @@
             //for 16 objects, randomly select for 8 objects and put a pair in
             for (int i = 0 ; i< gameCounter; i++){
                 int r = arc4random() % totalLines ;
+                if (![selectedSet containsObject:[NSNumber numberWithInt:r]])
+                {
+                    [selectedSet addObject:[NSNumber numberWithInt:r]];
+                }
+                else
+                {
+                    i--;
+                    continue;
+                }
                 NSString *word = [words objectAtIndex: r];
                 Content *wordAsContent = [[Content alloc] initWithWord:word];
                 [wordAsContent setMatchID:i];
@@ -113,15 +124,112 @@
         }
             break;
             
-        case 1:
+        case 1://wordtoSound
         {
-            return 0;
+            gameCounter = gameSize/2;
+            //////////////////////////////////loading words from list/////////////////////////////
+            NSMutableArray *words;
+            NSMutableArray *words_chosen;
+            words = [NSMutableArray array];
+            words_chosen = [NSMutableArray array];
+            NSMutableArray *selectedSet;
+            selectedSet = [NSMutableArray array];
+            //read file
+            NSString* path = [[NSBundle mainBundle] pathForResource:@"word_sound"
+                                                             ofType:@"lst"];
+            DDFileReader *reader = [[DDFileReader alloc] initWithFilePath:path];
+            NSString * line = nil;
+            //fetch input by lines
+            while ((line = [reader readLine])) {
+                [words addObject:line];
+            }
+            
+            NSUInteger totalLines = [words count];
+
+            for (int i = 0 ; i< gameCounter; i++){
+                int r = arc4random() % totalLines ;
+                if (![selectedSet containsObject:[NSNumber numberWithInt:r]])
+                {
+                    [selectedSet addObject:[NSNumber numberWithInt:r]];
+                }
+                else
+                {
+                    i--;
+                    continue;
+                }
+                NSString *word = [words objectAtIndex: r];
+                NSArray *items = [word componentsSeparatedByString:@":"];
+                Content *wordAsContent = [[Content alloc] initWithWord:[items objectAtIndex:0]];
+                Content *soundAsContent = [[Content alloc] initWithWord:[items objectAtIndex:1]];
+                [soundAsContent setHasSound:true];
+                [wordAsContent setMatchID:i];
+                [soundAsContent setMatchID:i];
+                [words_chosen addObject:wordAsContent];
+                [words_chosen addObject:soundAsContent];
+            }
+            //shuffling inside the words_chosen array
+            [words_chosen shuffle];
+            //put nil at the end
+            //[words_chosen addObject:nil];
+            
+            NSArray *gridContent = words_chosen;
+            return gridContent;
+
         }
             break;
             
-        case 2:
+        case 2://soundtoSound
         {
-            return 0;
+            gameCounter = gameSize/2;
+            //////////////////////////////////loading words from list/////////////////////////////
+            NSMutableArray *words;
+            NSMutableArray *words_chosen;
+            words = [NSMutableArray array];
+            words_chosen = [NSMutableArray array];
+            NSMutableArray *selectedSet;
+            selectedSet = [NSMutableArray array];
+            //read file
+            NSString* path = [[NSBundle mainBundle] pathForResource:@"word_sound"
+                                                             ofType:@"lst"];
+            DDFileReader *reader = [[DDFileReader alloc] initWithFilePath:path];
+            NSString * line = nil;
+            //fetch input by lines
+            while ((line = [reader readLine])) {
+                [words addObject:line];
+            }
+            
+            NSUInteger totalLines = [words count];
+            
+            for (int i = 0 ; i< gameCounter; i++){
+                int r = arc4random() % totalLines ;
+                if (![selectedSet containsObject:[NSNumber numberWithInt:r]])
+                {
+                    [selectedSet addObject:[NSNumber numberWithInt:r]];
+                }
+                else
+                {
+                    i--;
+                    continue;
+                }
+                NSString *word = [words objectAtIndex: r];
+                NSArray *items = [word componentsSeparatedByString:@":"];
+                Content *sound1 = [[Content alloc] initWithWord:[items objectAtIndex:1]];
+                Content *sound2 = [[Content alloc] initWithWord:[items objectAtIndex:1]];
+                [sound1 setHasSound:true];
+                [sound2 setHasSound:true];
+                [sound1 setMatchID:i];
+                [sound2 setMatchID:i];
+                [words_chosen addObject:sound1];
+                [words_chosen addObject:sound2];
+            }
+            //shuffling inside the words_chosen array
+            [words_chosen shuffle];
+            //put nil at the end
+            //[words_chosen addObject:nil];
+            
+            NSArray *gridContent = words_chosen;
+            return gridContent;
+
         }
             break;
             
@@ -156,6 +264,73 @@
             return gridContent;
         }
             break;
+            
+        case 4://braille
+        {
+            gameCounter = gameSize/2;
+            //////////////////////////////////loading words from list/////////////////////////////
+            NSMutableArray *words;
+            NSMutableArray *words_chosen;
+            words = [NSMutableArray array];
+            words_chosen = [NSMutableArray array];
+            NSMutableArray *selectedSet;
+            selectedSet = [NSMutableArray array];
+            //read file
+            NSString* path = [[NSBundle mainBundle] pathForResource:@"braille"
+                                                             ofType:@"lst"];
+            DDFileReader *reader = [[DDFileReader alloc] initWithFilePath:path];
+            NSString * line = nil;
+            //fetch input by lines
+            while ((line = [reader readLine])) {
+                [words addObject:line];
+            }
+            
+            NSUInteger totalLines = [words count];
+            
+            for (int i = 0 ; i< gameCounter; i++){
+                int r = arc4random() % totalLines ;
+                if (![selectedSet containsObject:[NSNumber numberWithInt:r]])
+                {
+                    [selectedSet addObject:[NSNumber numberWithInt:r]];
+                }
+                else
+                {
+                    i--;
+                    continue;
+                }
+                NSString *word = [words objectAtIndex: r];
+                NSArray *items = [word componentsSeparatedByString:@":"];
+                Content *character = [[Content alloc] initWithWord:[items objectAtIndex:0]];
+                Content *repre = [[Content alloc] initWithWord:[items objectAtIndex:1]];
+                [character setMatchID:i];
+                [repre setMatchID:i];
+                [words_chosen addObject:character];
+                [words_chosen addObject:repre];
+            }
+            //shuffling inside the words_chosen array
+            [words_chosen shuffle];
+            //put nil at the end
+            //[words_chosen addObject:nil];
+            
+            NSArray *gridContent = words_chosen;
+            return gridContent;
+
+            /*
+             [words_chosen_l addObject:character];
+             [words_chosen-r addObject:repre];
+             
+             [words_chosen_l shuffle];
+             [words_chosen_r shuffle];
+             
+             for ( int i =0 ; i < gameCounter/2; i++){
+                [words_chosen addObject: [words_chosen_l objectAtIndex: i]]
+                [words_chosen addObject: [words_chosen_r objectAtIndex: i]]
+             }
+             */
+            
+        }
+            break;
+            
             
         default:
         {
